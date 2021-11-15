373e84d3c35b15566aa0e5ee5345c1803ce51c45
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2021-11-16 01:07:01 +0200

    popup

---
+++ src/pages/PopUp/container.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PopUpMarkup from './markup'
+
+const PopUpContainer: React.FC = () => {
+    return <PopUpMarkup />
+}
+
+export default PopUpContainer
---
+++ src/pages/PopUp/markup.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PopUpMarkup: React.FC = () => {
+    return <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "80vw",
+        height: "80vh",
+        backgroundColor: "red",
+        margin: "0 auto",
+        marginTop: "100px"
+    }}>
+        Pop Up
+    </div>
+}
+
+export default PopUpMarkup
