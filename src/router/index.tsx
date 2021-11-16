956aa93e9e639f07ed2d592874c6e93218b23f5f
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2021-11-16 17:07:24 +0200

    router

---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+
+const Router = () => {
+    return <>
+        <Routes>
+            {/* <Route path="/home"/>
+            <Route path="/auth" element={Auth}/>
+            <Route path="/register"/> */}
+        </Routes>
+    </>
+}
