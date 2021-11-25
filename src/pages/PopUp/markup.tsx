27f31878e825462414d75807df80b2d7dd80615b
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2021-11-25 17:42:01 +0200

    pop up film details

--- src/pages/PopUp/markup.tsx
+++ src/pages/PopUp/markup.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import JsonData from '../../data/imdb.json'
 import { useNavigate, useParams } from 'react-router'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 interface IPopUp {
     animate: any
@@ -10,10 +11,31 @@ const PopUpMarkup: React.FC<IPopUp> = ({ animate }) => {
     const params = useParams()
     const navigate = useNavigate()
 
-    const { title } = JsonData.filter(el => el.id === params.id)[0]
+    const { title, poster, writer, actors, imdbrating } = JsonData.filter(el => el.id === params.id)[0]
 
     return <div style={styles.container}>
-        <span onClick={() => navigate(-1)}>{title}</span>
+        <div>
+            <img
+                width="450px"
+                height="600px"
+                src={poster}
+                alt="poster"
+            />
+        </div>
+        <div style={styles.details}>
+            <h1>{title}</h1>
+            <span>Writer: {writer}</span>
+            <span>Actors: {actors}</span>
+            <span>Rating: {imdbrating}</span>
+        </div>
+        <div style={styles.iconWrapper}>
+            <i style={styles.getBackArrow}
+                onClick={() => navigate(-1)}
+            >
+                <ArrowBackIcon />
+            </i>
+        </div>
+
     </div>
 }
 
@@ -22,12 +44,41 @@ export default PopUpMarkup
 const styles = {
     container: {
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "80vw",
-        height: "80vh",
+        position: "relative" as "relative",
+        width: "1000px",
+        height: "600px",
         backgroundColor: "red",
         margin: "0 auto",
-        marginTop: "100px"
+        marginTop: "100px",
+        color: "white"
+    },
+    details: {
+        paddingTop: "30px",
+        paddingLeft: "30px",
+        display: "flex",
+        justifyContent: 'flex-start',
+        alignItems: "flex-start",
+        flexDirection: "column" as "column"
+    },
+    getBackArrow: {
+
+        // position: "absolute" as "absolute",
+        // top: "30px",
+        // left: "30px",
+        opacity: 1,
+        color: "white",
+    },
+    iconWrapper: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "absolute" as "absolute",
+        top: "30px",
+        left: "40px",
+        width: "40px",
+        height: "30px",
+        backgroundColor: "gray",
+        // opacity: 0.5,
+        borderRadius: 4
     }
 }
