7d847b704cfecb85dff6b95d2780d46120b52641
Dmytro Serdiukov <dmytro.serdiukov@gmail.com>
2021-11-28 02:16:49 +0200

    welcome screen styles refactor

--- src/pages/WelcomeScreen/index.tsx
+++ src/pages/WelcomeScreen/index.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
+
 import Button from '../../components/Button'
+
+import styles from './styles.module.css'
 
 const WelcomeScreen: React.FC = () => {
     const navigate = useNavigate()
     return (
-        <div style={{
-            width: "100vw",
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center"
-        }}>
+        <div className={styles.container}>
             <Button
                 margin={"20px 5px"}
                 width={350}
